505f506d6704531ff5c5d7eb7c10bf9e4016210e
Victor Tran <victortran415@gmail.com>
2020-11-30 13:33:55 -0800

    testing some questions

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
 // array of questions for user
-const questions = [
 
+//NOTE: testing examples for now. will input actual questions later
+const questions = [
+    {
+        type: "input",
+        message: "Enter your name",
+        name: "nameInput",
+    },
+    {
+        type: "input",
+        message: "how old are you?",
+        name: "ageInput",
+    },
+    {
+        type: "input",
+        message: "what language do you know?",
+        name: "languageInput",
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.watchFile(fileName, data, (err) => {
+        if (err) throw (err);
+        console.log("testing")
+    })
 }
 
 // function to initialize program
