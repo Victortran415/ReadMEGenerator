0ec66af50caedb8457c4f200b73c9f6201cdc46e
Victor Tran <victortran415@gmail.com>
2020-11-30 18:46:39 -0800

    changed to better questions

--- index.js
+++ index.js
@@ -2,25 +2,47 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 // array of questions for user
 
-//NOTE: testing examples for now. will input actual questions later
+//UPDATE: questions changed to something related to a high-quality readMe.
 const questions = [
     {
         type: "input",
-        message: "Enter your name",
-        name: "nameInput",
+        message: "What is the title of your project?",
+        name: "projectInput"
     },
     {
         type: "input",
-        message: "how old are you?",
-        name: "ageInput",
+        message: "Describe the purpose of your application",
+        name: "description"
     },
     {
         type: "input",
-        message: "what language do you know?",
-        name: "languageInput",
-    }
+        message: "What was installed in order to create this readMe?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Add a demo(video or gif) of how to use this generator and explain the steps.",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "License for your project",
+        choices:  ['Apache License', ' MIT License', ' Mozilla Public License 2.0', ' GPL License'],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Enter your Github User",
+        name: "contact"
+    },
+    {
+        type: "input",
+        message: "Enter your email addrerss",
+        name: "contact"
+    },
 ];
 
+//NOTE: will get back to this.
 // function to write README file
 function writeToFile(fileName, data) {
     fs.watchFile(fileName, data, (err) => {
@@ -28,6 +50,7 @@ function writeToFile(fileName, data) {
         console.log("testing")
     })
 }
+
 
 // function to initialize program
 function init() {
