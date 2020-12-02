c8efd6985bc363b89e2c11f24aac320ca0a407c3
Victor Tran <victortran415@gmail.com>
2020-12-01 16:35:16 -0800

    adding codes to generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,6 +9,56 @@ function generateMarkdown(response) {
 
   //Time of Content(installation, usage, license, contribution, tests, questions)
 
+  //NOTE: may be doing this incorrectly, i'll get back to it.
+  let ToC = `## Time of Contents`
+  if (response.installation) {
+    ToC += `- [Installation](#installation)`;
+  }
+  if (response.usage) {
+    ToC += `- [Usage](#usage)`;
+  }
+  if (response.license) {
+    ToC += `- [License](#license)`
+  } else {
+    ToC
+  }
+  if (response.contribution) {
+    ToC += `- [Contribution](#contribution)`;
+  }
+  if (response.tests) {
+    ToC += `- [Tests](#tests)`;
+  }
+  if (response.contact) {
+    ToC += `- [Contact](#contact)`
+  }
+  console.log(ToC)
+
+  //Installations
+  `## Installation:
+  ${response.installation}`
+
+  //Usage
+
+  `## Usage:
+  ${response.usage}`
+
+  //License
+  `## License
+  ${response.license}`
+
+  //Contribution
+  `## Contribution
+  ${response.contribution}`
+
+  //Tests
+  `## Tests
+  ${response.tests}`
+
+  //Questions
+  `## Questions
+  ${response.contact}`
+
+
 return response
 }
 
